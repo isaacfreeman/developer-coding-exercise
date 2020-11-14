80c98245343b6bcc054091fc1af5139594a928c7
Isaac Freeman <isaac@freeman.org.nz>
2020-11-14 14:04:11 +1300

    Implement Posts page

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,36 @@ import React from "react"
 import PostsPage from "./PostsPage"
 import PostPage from "./PostPage"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { QueryCache, ReactQueryCacheProvider } from "react-query"
+
+const queryCache = new QueryCache()
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/post/foo">Post</Link>
-            </li>
-          </ul>
-        </nav>
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/post/foo">Post</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Switch>
-          <Route path="/post/:slug">
-            <PostPage />
-          </Route>
-          <Route path="/">
-            <PostsPage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/post/:slug">
+              <PostPage />
+            </Route>
+            <Route path="/">
+              <PostsPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ReactQueryCacheProvider>
   )
 }
--- client/src/PostsPage/PostsPage.js
+++ client/src/PostsPage/PostsPage.js
@@ -1,11 +1,37 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useQuery } from "react-query"
+
+async function fetchPosts() {
+  const res = await fetch(
+    "http://localhost:4000/posts"
+  )
+  return await res.json()
+}
 
 export default function PostsPage() {
+  const { data, status, error } = useQuery("myQueryKey", fetchPosts)
+
+  if (status === "loading") return "Loading..."
+
+  if (error) return "An error has occurred: " + error.message
+
   return (
     <div>
       <h2>Posts</h2>
-      <Link to="/post/foo">Foo</Link>
+      <ul>
+        {data.map((post, index) => {
+          return (
+            <React.Fragment key={index}>
+              <li>
+                <Link to={`/post/${post.slug}`}>
+                  {post.title}
+                </Link>
+              </li>
+            </React.Fragment>
+          )
+        })}
+      </ul>
     </div>
   )
 }
